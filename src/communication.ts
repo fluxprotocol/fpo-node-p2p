eec5329a5c2eb4a9d33ffb43adf784fb8433e025
gluax <jonathan.t.pavlik@gmail.com>
2022-04-07 09:37:48 -0700

    initial commit

---
+++ src/communication.ts
@@ -0,0 +1,212 @@
+import Libp2p, { Connection, create, CreateOptions, Libp2pOptions } from "libp2p";
+import TCP from "libp2p-tcp";
+const Mplex = require("libp2p-mplex"); // no ts support yet :/
+import { NOISE } from "@chainsafe/libp2p-noise";
+import { Multiaddr } from "multiaddr";
+import PeerId from "peer-id";
+import BufferList from "bl/BufferList";
+import { existsSync, readFileSync, writeFileSync } from "fs";
+
+import logger from "./logger";
+
+async function* createAsyncIterable(syncIterable: Uint8Array[]) {
+	for (const elem of syncIterable) {
+		yield elem;
+	}
+}
+
+async function attempt<R>(comm: Communicator, def: R, attempt: (node: Libp2p) => Promise<R>): Promise<R> {
+	if (comm._node === undefined) {
+		logger.error('The communication node has not been started.');
+		return def;
+	} else {
+		return attempt(comm._node);
+	}
+}
+
+export default class Communicator {
+	_connections: Set<Connection> = new Set();
+	_options: Libp2pOptions & CreateOptions;
+	_node?: Libp2p;
+	_peers: Set<string>;
+	_peers_file: string;
+	_retry: Set<string> = new Set();
+	_protocol: string;
+
+	constructor(protocol: string, config?: Libp2pOptions & CreateOptions, peers?: string) {
+		if (config === undefined) {
+			this._options = {
+				addresses: {
+					listen: ['/ip4/0.0.0.0/tcp/0']
+				},
+				modules: {
+					transport: [TCP],
+					streamMuxer: [Mplex],
+					connEncryption: [NOISE],
+				}
+			};
+		} else {
+			this._options = config;
+		}
+
+		if (peers === undefined) {
+			this._peers_file = 'peers.json';
+			this._peers = new Set();
+		} else {
+			this._peers_file = peers;
+			this._peers = this.load_peers();
+		}
+
+
+		this._protocol = protocol;
+	}
+
+	async init(): Promise<void> {
+		this._node = await create(this._options);
+	}
+
+	async retry(): Promise<void> {
+		attempt(this, null, async () => {
+			for (const peer of this._retry) {
+				if (await this.connect(new Multiaddr(peer))) {
+					this._retry.delete(peer);
+				}
+			}
+		});
+	}
+
+	async start(): Promise<[Multiaddr, PeerId] | void> {
+		return attempt(this, undefined, async (node: Libp2p) => {
+			await node.start();
+
+			for (const peer of this._peers) {
+				if (!await this.connect(new Multiaddr(peer))) {
+					this._retry.add(peer);
+				}
+			}
+
+			return [
+				node.multiaddrs[0],
+				node.peerId,
+			];
+		})
+	}
+
+	async stop(): Promise<void> {
+		attempt(this, false, async (node: Libp2p) => {
+			try {
+				for (const peer of this._peers) {
+					node.hangUp(peer);
+				}
+
+				this.save_peers();
+
+				await node.stop();
+
+				return true;
+			} catch (error) {
+				logger.error(`Node failed to stop with error '${error}'.`);
+				return false;
+			}
+		});
+	}
+
+	async connect(ma: Multiaddr): Promise<boolean> {
+		return attempt(this, false, async (node: Libp2p) => {
+			try {
+				this._peers.add(ma.toString());
+				const conn = await node.dial(ma);
+				if (conn !== undefined) {
+					this._connections.add(conn);
+					return true;
+				}
+
+				return false;
+			} catch (error) {
+				logger.error(`Node failed to connect to ${ma} with error '${error}'.`);
+				return false;
+			}
+		});
+
+	}
+
+	async connect_from_details(ip: string, address: string, transport: string, port: string, peerID: string): Promise<boolean> {
+		return attempt(this, false, async (node: Libp2p) => {
+			const mas = `/${ip}/${address}/${transport}/${port}/p2p/${peerID}`;
+			const ma = new Multiaddr(`/${ip}/${address}/${transport}/${port}/p2p/${peerID}`);
+
+			try {
+				if (!this._peers.has(mas)) {
+					this._peers.add(mas);
+				}
+
+				const conn = await node.dial(ma);
+				if (conn !== undefined) {
+					this._connections.add(conn);
+					return true;
+				}
+
+				return false;
+			} catch (error) {
+				this._peers.delete(mas);
+				logger.error(`Node failed to connect to ${ma} with error '${error}'.`);
+				return false;
+			}
+		});
+	}
+
+	async handle_incoming(callback: (source: AsyncIterable<Uint8Array | BufferList>) => Promise<void>): Promise<void> {
+		await attempt(this, null, async (node: Libp2p) => {
+			node.handle(this._protocol, async ({ stream }) => {
+				await callback(stream.source);
+			});
+		});
+	}
+
+	async send(data: Uint8Array[]): Promise<void> {
+		attempt(this, null, async () => {
+			for (const connection of this._connections) {
+				const { stream } = await connection.newStream(this._protocol);
+				await stream.sink(createAsyncIterable(data));
+			}
+		});
+	}
+
+	save_peers(): void {
+
+		let peers = [];
+		for (const peer of this._peers) {
+			peers.push(peer.toString());
+		}
+
+		const json = JSON.stringify(peers, null, 4);
+		try {
+			writeFileSync(this._peers_file, json);
+		} catch (error) {
+			logger.error(`Node failed to save peers with error '${error}'.`);
+		}
+		
+	}
+
+	load_peers(): Set<string> {
+		if (!existsSync(this._peers_file)) {
+			return new Set();
+		}
+
+		try {
+			const json = readFileSync(this._peers_file, 'utf-8');
+			const peers_list: string[] = JSON.parse(json);
+
+			let peers: Set<string> = new Set();
+			for (const peer of peers_list) {
+				peers.add(peer);
+			}
+
+			return peers;
+		} catch (error) {
+			logger.error(`Node failed to load peers with error '${error}'.`);
+			return new Set();
+		}
+
+	}
+}
---
+++ src/logger.ts
@@ -0,0 +1,77 @@
+import winston, { format } from 'winston';
+import { DEBUG, ENABLE_ANALYTICS, MAX_LOG_LIFETIME, NODE_ID, PROJECT_NAME, PROJECT_VERSION, SENTRY_DSN } from './config';
+import Sentry from 'winston-transport-sentry-node';
+import 'winston-daily-rotate-file';
+
+const logFormat = format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`);
+
+const logger = winston.createLogger({
+    format: format.combine(
+        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' }),
+        format.metadata({ fillExcept: ['message', 'level', 'timestamp', 'label'] }),
+    ),
+    transports: [
+        new winston.transports.DailyRotateFile({
+            level: 'debug',
+            filename: `${PROJECT_NAME}-%DATE%.log`,
+            datePattern: 'YYYY-MMM-DD',
+            zippedArchive: true,
+            dirname: `logs/`,
+            format: logFormat,
+            maxFiles: MAX_LOG_LIFETIME,
+        }),
+        new winston.transports.Console({
+            level: 'info',
+            format: format.combine(
+                format.colorize(),
+                logFormat,
+            ),
+        }),
+    ],
+});
+
+if (ENABLE_ANALYTICS) {
+    logger.add(new Sentry({
+        sentry: {
+            dsn: SENTRY_DSN,
+            release: PROJECT_VERSION,
+            serverName: NODE_ID,
+            beforeSend: (event, hint) => {
+                const message = hint?.originalException?.toString();
+
+                if (message) {
+                    event.fingerprint = [message];
+                }
+
+                if (event.extra) {
+                    const metadata = event.extra.metadata as any;
+
+                    // Overwrite Sentry fingerprint
+                    if (metadata?.fingerprint) {
+                        event.fingerprint = [metadata.fingerprint];
+                    }
+
+                    if (metadata && metadata.config) {
+                        if (typeof metadata.config !== 'string') {
+                            metadata.config = JSON.stringify(metadata.config);
+                        }
+
+                        metadata.c64 = Buffer.from(metadata.config as string).toString('base64');
+                    }
+                }
+
+                return event;
+            }
+        },
+        level: 'warn',
+    }));
+}
+
+logger.transports.forEach((transport) => {
+    // @ts-ignore
+    if (transport.name === 'console') {
+        transport.level = DEBUG ? 'debug' : 'info';
+    }
+});
+
+export default logger;
